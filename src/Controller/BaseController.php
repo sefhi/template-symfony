8eb9c4eaffc7d23034a6c5c69349dff5331726ff
sefhirot69 <josalillo@gmail.com>
2023-06-20 19:42:12 +0200

    BREAKING CHANGE!: remove folder shared

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
-use App\Shared\ExceptionsHttpStatusCodeMapping;
+use Shared\Infrastructure\Exceptions\ExceptionsHttpStatusCodeMapping;
 
 use function Lambdish\Phunctional\each;
 
