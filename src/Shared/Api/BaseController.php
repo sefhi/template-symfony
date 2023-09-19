1b86dea5ee0eb6f46f90e677c7f2d8e8334bece3
sefhirot69 <josalillo@gmail.com>
2023-09-19 21:29:04 +0200

    Feat: add deserialize dto and validation dto

--- src/Shared/Api/BaseController.php
+++ src/Shared/Api/BaseController.php
@@ -11,8 +11,14 @@ use App\Shared\Domain\Bus\Query\Query;
 use App\Shared\Domain\Bus\Query\QueryBusInterface;
 use App\Shared\Domain\Bus\Query\QueryResponse;
 use App\Shared\Infrastructure\Exceptions\SymfonyExceptionsHttpStatusCodeMapping;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 use function Lambdish\Phunctional\each;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\Validator\Exception\ValidationFailedException;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 abstract class BaseController extends AbstractController
 {
@@ -20,6 +26,8 @@ abstract class BaseController extends AbstractController
         SymfonyExceptionsHttpStatusCodeMapping $exceptionMapping,
         protected readonly CommandBusInterface $commandBus,
         protected readonly QueryBusInterface $queryBus,
+        protected readonly SerializerInterface $serializer,
+        protected readonly ValidatorInterface $validator,
     ) {
         each(
             fn (int $httpCode, string $exceptionClass) => $exceptionMapping->register($exceptionClass, $httpCode),
@@ -35,6 +43,38 @@ abstract class BaseController extends AbstractController
     public function dispatch(Command $command): ?CommandResponse
     {
         return $this->commandBus->dispatch($command);
+    }
+
+    protected function deserialize(Request $request, string $class): mixed
+    {
+        $content = null;
+
+        if ('json' === $request->getContentTypeFormat() || !empty($request->getContent())) {
+            $content = $request->getContent();
+        } elseif (!empty($request->request->all())) {
+            $content = json_encode($request->request->all(), JSON_THROW_ON_ERROR);
+        } elseif (!empty($request->query->all())) {
+            $content = json_encode($request->query->all(), JSON_THROW_ON_ERROR);
+        }
+
+        return $this->serializer->deserialize(
+            $content,
+            $class,
+            'json'
+        );
+    }
+
+    protected function validationRequest(Request $request, string $class): mixed
+    {
+        $objectDto = $this->deserialize($request, $class);
+
+        $validationErrors = $this->validator->validate($objectDto);
+
+        if ($validationErrors->count() > 0) {
+            throw new ValidationFailedException($objectDto::class, $validationErrors);
+        }
+
+        return $objectDto;
     }
 
     abstract protected function exceptions(): array;
