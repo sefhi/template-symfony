58552ae825c1c34e0b340b255ac419a9717de36a
jmalias <jm.alias@covermanager.com>
2025-03-25 09:08:42 +0100

    BREAKING CHANGE!: Add transaction handling to BaseApiTestCase.

--- tests/Functional/BaseApiTestCase.php
+++ tests/Functional/BaseApiTestCase.php
@@ -4,16 +4,26 @@ declare(strict_types=1);
 
 namespace Tests\Functional;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class BaseApiTestCase extends WebTestCase
 {
     private KernelBrowser $client;
+    private EntityManagerInterface $entityManager;
 
     protected function setUp(): void
     {
-        $this->client = self::createClient();
+        $this->entityManager = self::getContainer()->get('doctrine.orm.entity_manager');
+        $this->client        = self::createClient();
+
+        $this->entityManager->beginTransaction();
+    }
+
+    protected function tearDown(): void
+    {
+        $this->entityManager->rollback();
     }
 
     public function client(): KernelBrowser
